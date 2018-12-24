f176151da55f908e7565a8c78dc9973f1709c644
Yasmin Torres <yasmintorres90@hotmail.com>
2018-12-24 13:16:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-var cart = [];
+ var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -22,43 +21,45 @@ function getCart() {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random() * 100);
-  cart.push({[item]: price});
+  var obj = {};
+  obj[item] = Math.floor(Math.random() * 100);
+  cart.push(obj);
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
 
 function viewCart() {
-  if (cart.length === 0) {
+  if(cart.length === 0)
     console.log("Your shopping cart is empty.");
-  } else {
-    var cartList = [];
-    for (var i = 0; i < cart.length; i++) {
-      var itemKey = Object.keys(cart[i])[0];
-      var itemValue = cart[i][itemKey];
-      cartList.push(`${itemKey} at $${itemValue}`);
+  else {
+    var msg = 'In your cart, you have';
+    for(var i = 0; i < cart.length; i++) {
+      if(i === 0)
+        msg+= ` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`;
+      else
+        msg+= `, ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`;
     }
-    console.log("In your cart, you have " + cartList.join(", ") + ".");
+    msg+='.';
+    console.log(msg);
   }
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item)) {
-      cart.splice(i, 1);
-      return cart;
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i][item] !== undefined) {
+      cart.splice(i,1);
+      return;
     }
   }
-  console.log("That item is not in your cart.");
+  console.log('That item is not in your cart.');
 }
 
-function placeOrder(cardNumber) {
-  if(cardNumber === parseInt(cardNumber)) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    while(cart.length > 0) {
-      cart.pop();
-    }
-  } else {
+function placeOrder(creditCard) {
+  if(creditCard === undefined)
     console.log("We don't have a credit card on file for you to place your order.");
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${creditCard}.`);
   }
+
+  cart = [];
 }
